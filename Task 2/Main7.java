52c45a2a26af75aa1e19c019dcd1c985e24589a9
Dwarm-A <abdurrahimarmaan058@gmail.com>
2026-06-18 16:57:13 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,16 @@
+interface Clickable{
+    void oneclick();
+    }
+class Button implements Clickable{
+        
+    public void oneclick(){
+        System.out.println("Button was clicked");
+    }
+}
+public class Main7 {
+    public static void main(String[] args) {
+        Button haaland = new Button();
+        haaland.oneclick();
+    }
+}
 
