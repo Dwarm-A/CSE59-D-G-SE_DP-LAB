3b8fb7707dae837f422e822bf340dda68b098f4f
Dwarm-A <abdurrahimarmaan058@gmail.com>
2026-06-18 16:43:09 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,16 @@
-
+class Printer{
+    void printData(String text){
+        System.out.println(text);
+    }
+    void printData(int number){
+        System.out.println(number);
+    }
+}
+public class Main5 {
+    public static void main(String[] args) {
+        Printer donald = new Printer();
+        donald.printData("Good Evening");
+        donald.printData(420);
+             
+    }
+}
