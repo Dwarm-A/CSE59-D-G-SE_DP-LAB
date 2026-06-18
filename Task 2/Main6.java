e3e4976786a228ef1d6f65b6eb0ff1c0e4d07613
Dwarm-A <abdurrahimarmaan058@gmail.com>
2026-06-18 16:51:00 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,18 @@
-
+class Applet{
+    void launch(){
+        System.out.println("Applet launched");
+    }
+}
+class GameApplet extends Applet{
+    void play(){
+        System.out.println("Playing game");
+    }
+}
+public class Main6 {
+    public static void main(String[] args) {
+        GameApplet Armaan = new GameApplet();
+        Armaan.launch();
+        Armaan.play();
+    }
+    
+}
